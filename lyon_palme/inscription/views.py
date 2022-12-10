06d02d0233eb001c4a22acb184f619359beec90c
tom <tomperrat@gmail.com>
2022-12-10 12:15:59 +0100

    ajout des regex mail, tel, cp, fonctionnement à tester avec formulaire d'inscription

--- lyon_palme/inscription/views.py
+++ lyon_palme/inscription/views.py
@@ -7,13 +7,26 @@ from .forms import Formulaire_inscription
 def inscription_form(request):
     if request.method == 'POST':
         form = Formulaire_inscription(request.POST)
-        if form.is_valid() and Regex.verif_mail(form.cleaned_data['nom']):
+        if form.is_valid() and Regex.verif_mail(form.cleaned_data['mail']) and Regex.verif_tel(form.cleaned_data['tel']) and Regex.verif_cp(form.cleaned_data['cp']):#REMPLACER LES NOMS ENTRE GUILLEMETS PAR LES NOMS DES CASES DU FORMULAIRE 
             return render(request, 'inscription/inscription_form.html', {'form' : form})
         else:
             erreur_mail = ""
-            if not Regex.verif_mail(form.cleaned_data['nom']):
+            erreur_tel = ""
+            erreur_cp = ""
+
+            if not Regex.verif_mail(form.cleaned_data['mail']):#REMPLACER LES NOMS ENTRE GUILLEMETS PAR LES NOMS DES CASES DU FORMULAIRE 
                 erreur_mail = "Mauvais format d'adresse mail"
-            return render(request, 'inscription/inscription_form.html', {'form' : form, 'erreur_mail' : erreur_mail})
+            if not Regex.verif_tel(form.cleaned_data['tel']):
+                erreur_mail = "Mauvais format de numéro de téléphone : les chiffres doivent être séparés par des points, des tirets ou des espaces"
+            if not Regex.verif_cp(form.cleaned_data['cp']):
+                erreur_mail = "Mauvais format de code postal"
+            context = {
+                'form' : form,
+                'erreur_mail' : erreur_mail,
+                'erreur_tel' : erreur_tel,
+                'erreur_cp' : erreur_cp
+            }
+            return render(request, 'inscription/inscription_form.html', context)
     else:
         form = Formulaire_inscription()
     
