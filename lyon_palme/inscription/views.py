55b91461041b1d8532e21f588fe5a3a67c457892
tom <tomperrat@gmail.com>
2022-12-10 22:27:29 +0100

    retiré des commentaires inutiles

--- lyon_palme/inscription/views.py
+++ lyon_palme/inscription/views.py
@@ -9,7 +9,7 @@ from .models import Inscription
 def inscription_form(request):
     if request.method == 'POST':
         form = Formulaire_inscription(request.POST)
-        if form.is_valid() and Regex.verif_mail(form.cleaned_data['mail']) and Regex.verif_tel(form.cleaned_data['telephone']) and Regex.verif_cp(form.cleaned_data['code_postal']):#REMPLACER LES NOMS ENTRE GUILLEMETS PAR LES NOMS DES CASES DU FORMULAIRE 
+        if form.is_valid() and Regex.verif_mail(form.cleaned_data['mail']) and Regex.verif_tel(form.cleaned_data['telephone']) and Regex.verif_cp(form.cleaned_data['code_postal']):
             reussi = "réussi"
             adherent = Inscription()
             adherent.nom = request.POST['nom']
@@ -27,7 +27,7 @@ def inscription_form(request):
             erreur_tel = ""
             erreur_cp = ""
 
-            if not Regex.verif_mail(form.cleaned_data['mail']):#REMPLACER LES NOMS ENTRE GUILLEMETS PAR LES NOMS DES CASES DU FORMULAIRE 
+            if not Regex.verif_mail(form.cleaned_data['mail'])
                 erreur_mail = "Mauvais format d'adresse mail"
             if not Regex.verif_tel(form.cleaned_data['telephone']):
                 erreur_tel = "Mauvais format de numéro de téléphone : les chiffres doivent être séparés par des points, des tirets ou des espaces"
