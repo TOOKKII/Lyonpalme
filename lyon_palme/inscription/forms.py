4ddfef9a70a8e9f9e1844ad639041695f76de50c
tom <tomperrat@gmail.com>
2022-12-10 12:15:16 +0100

    correction du nom de modele

--- lyon_palme/inscription/forms.py
+++ lyon_palme/inscription/forms.py
@@ -3,5 +3,5 @@ from django import forms
 import inscription
 
 class Formulaire_inscription(forms.ModelForm):#FINIR FORMULAIRE ERREUR POUR L'INSTANT
-    model = inscription
+    model = Inscription
     fields = '__all__'
